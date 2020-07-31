c89a47d376639f3e8fa42e27bff747df7214a5ea
jfischeryaro <59482756+jfischeryaro@users.noreply.github.com>
2020-07-31 15:59:10 -0500

    added docker file

---
+++ database/init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+POSTGRES="psql --username ${POSTGRES_USER}"
+
+echo "Creating database: ${POSTGRES_DB}"
+
+$POSTGRES <<EOSQL
+CREATE DATABASE ${POSTGRES_DB} OWNER ${POSTGRES_USER};
+EOSQL
+
+echo "Creating schema..."
+psql -d ${POSTGRES_DB} -a -U${POSTGRES_USER} -f /schema.sql
---
+++ database/schema.sql
@@ -0,0 +1,3 @@
+CREATE TABLE "jsontable" (
+    "jsontest" varchar
+)
